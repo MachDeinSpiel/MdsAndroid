6fa9df6e16aca53919454b101a9ccb4c456cda37
nhatje <nhatje@mx.de>
2014-03-22 13:34:18 +0100

    EventParser bearbeitet, noch nicht komplett! Überlegung, ob der EventParser in TransitionParser umbenannt werden sollte? Da er ja jetzt auch Conditions überprüft, ohne Events...

--- MdsAndroid/src/de/hsbremen/mds/interpreter/EventParser.java
+++ MdsAndroid/src/de/hsbremen/mds/interpreter/EventParser.java
@@ -1,5 +1,84 @@
 package de.hsbremen.mds.interpreter;
 
-public class EventParser {
+import java.util.ArrayList;
+import java.util.List;
 
+import android.location.Location;
+import de.hsbremen.mds.common.valueobjects.statemachine.MdsItem;
+import de.hsbremen.mds.common.valueobjects.statemachine.MdsTransition;
+
+public class EventParser {
+	
+	public MdsTransition checkEvents(List<MdsItem> items, Location loc, List<MdsTransition> transitions) {
+		
+		for(MdsTransition t : transitions) {
+			// alle mit Typ gameEvent
+			if (t.getEvent().getType().equals("gameEvent")) {
+				if(t.getEvent().getName().equals("nearby")) {
+					int radius = Integer.parseInt(t.getEvent().getParams().get("radius"));
+					// alle Items durchgehen und gucken ob genug vorhanden sind
+					int quanti = Integer.parseInt(t.getEvent().getParams().get("quantifier"));
+					if (checkLocationItems(items, loc, radius).size() >= quanti) {
+						return t;
+					}
+				}
+			} else if (t.getEvent().getType().equals("condition")) {
+				// get value and compValue
+				double value = -1;
+				double compValue = -2;
+				try {  
+				    value = Double.parseDouble(t.getEvent().getParams().get("value"));  
+				} catch(NumberFormatException nfe) {  
+					// TODO: was wenn da dynamische Werte drin sind? 
+				} 
+				try {  
+				    compValue = Double.parseDouble(t.getEvent().getParams().get("compValue"));  
+				} catch(NumberFormatException nfe) {  
+					// TODO: was wenn da dynamische Werte drin sind? 
+				} 
+				
+				// get checkType
+				if (t.getEvent().getParams().get("checkType").equals("==")) {
+					if (value == compValue) {
+						return t;
+					}
+				} else if (t.getEvent().getParams().get("checkType").equals("<")) {
+					if (value < compValue) {
+						return t;
+					}
+				} else if (t.getEvent().getParams().get("checkType").equals(">")) {
+					if (value > compValue) {
+						return t;
+					}
+				} else if (t.getEvent().getParams().get("checkType").equals("<=")) {
+					if (value <= compValue) {
+						return t;
+					}
+				} else if (t.getEvent().getParams().get("checkType").equals(">=")) {
+					if (value >= compValue) {
+						return t;
+					}
+				}
+			}
+		}
+		// wenn kein Event gestimmt hat
+		return null;	
+	}
+	
+	private List<MdsItem> checkLocationItems(List<MdsItem> items, Location loc, int radius ) {
+		
+		Location pos = new Location(loc);
+		List<MdsItem> result = new ArrayList<MdsItem>();
+		
+		for (MdsItem i : items) {
+			// TODO: Position für Items erzeugen?
+			Location itemPos = new Location("i.itemPos");
+			if(itemPos.distanceTo(pos) <= radius) {
+				result.add(i);
+			}
+		}
+		return result;
+		
+	}
+	
 }
