3d034a37504b7e92553df20c9b25f88d01e8bda8
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-03-22 10:44:45 +0100

    Classen jelöscht

--- MdsAndroid/src/de/hsbremen/mds/interpreter/exceptions/InvalidFsmException.java
+++
@@ -1,8 +0,0 @@
-package de.hsbremen.mds.interpreter.exceptions;
-
-
-public class InvalidFsmException extends Exception {
-	public InvalidFsmException(){
-		super("The Finit State Machine is invalid");
-	}
-}
