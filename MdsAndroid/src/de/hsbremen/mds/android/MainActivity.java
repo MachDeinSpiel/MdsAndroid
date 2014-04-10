9ed3d4ea17b432afd730259095296073e17a6744
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-04-10 13:34:49 +0200

    FsmManager übergibt die Eventsjetzt richtig an den EventParser

--- MdsAndroid/src/de/hsbremen/mds/android/MainActivity.java
+++ MdsAndroid/src/de/hsbremen/mds/android/MainActivity.java
@@ -26,6 +26,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import de.hsbremen.mds.android.listener.AndroidInitiater;
+import de.hsbremen.mds.common.whiteboard.Whiteboard;
 import de.hsbremen.mds.common.interfaces.GuiInterface;
 import de.hsbremen.mds.common.listener.AndroidListener;
 import de.hsbremen.mds.common.valueobjects.MdsImage;
--- MdsAndroid/src/de/hsbremen/mds/interpreter/Interpreter.java
+++ MdsAndroid/src/de/hsbremen/mds/interpreter/Interpreter.java
@@ -7,6 +7,7 @@ import java.util.Vector;
 import android.util.Log;
 import de.hsbremen.mds.common.interfaces.FsmInterface;
 import de.hsbremen.mds.common.interfaces.ClientInterpreterInterface;
+import de.hsbremen.mds.common.interfaces.FsmInterface;
 import de.hsbremen.mds.common.interfaces.GuiInterface;
 import de.hsbremen.mds.common.interfaces.InterpreterInterface;
 import de.hsbremen.mds.common.interfaces.ServerInterpreterInterface;
@@ -14,14 +15,13 @@ import de.hsbremen.mds.common.listener.AndroidListener;
 import de.hsbremen.mds.common.valueobjects.statemachine.MdsObjectContainer;
 import de.hsbremen.mds.common.valueobjects.statemachine.MdsState;
 import de.hsbremen.mds.common.whiteboard.Whiteboard;
-import de.hsbremen.mds.common.whiteboard.Whiteboard;
 import de.hsbremen.mds.common.whiteboard.WhiteboardEntry;
 import de.hsbremen.mds.parser.Parser;
 
 /**
  * @author JW
  */
-public class Interpreter implements InterpreterInterface, AndroidListener, ClientInterpreterInterface{
+public class Interpreter implements InterpreterInterface, AndroidListener, ClientInterpreterInterface, FsmInterface{
 	private ActionParser actionParser;
 	
 	private Whiteboard whiteboard;
@@ -96,4 +96,12 @@ public class Interpreter implements InterpreterInterface, AndroidListener, Clien
 		this.whiteboard = newWhiteboard;
 		
 	}
+
+	@Override
+	public void onStateChange() {
+		// TODO ActionParser bescheid sagen.
+		
+	}
+
+
 }
