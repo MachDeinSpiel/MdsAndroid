de5b5874eea702f985a4cb7f5b237b72caacaad5
Cedric <dcedric@stud.hs-bremen.de>
2014-04-15 18:07:17 +0200

    Fehlende packages importet -.-

--- MdsAndroid/src/de/hsbremen/mds/parser/ParserServer.java
+++ MdsAndroid/src/de/hsbremen/mds/parser/ParserServer.java
@@ -5,10 +5,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import de.hsbremen.mds.common.interfaces.InterpreterInterface;
 import de.hsbremen.mds.common.valueobjects.statemachine.MdsExhibit;
 
 
